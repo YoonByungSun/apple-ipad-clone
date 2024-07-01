e5bd0ca4c504ed1048f5c7c4fae9a07f5302e08f
YoonByungSun <ssp31n@naver.com>
2024-07-01 22:51:11 +0900

    Add Header

--- js/main.js
+++ js/main.js
@@ -35,6 +35,8 @@ const searchWrapEl = headerEl.querySelector('.search-wrap');
 const searchStarterEl = headerEl.querySelector('.search-starter');
 const searchCloserEl = searchWrapEl.querySelector('.search-closer');
 const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchInputEl = searchWrapEl.querySelector('input');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
 
 searchStarterEl.addEventListener('click', showSearch);
 searchCloserEl.addEventListener('click', hideSearch);
@@ -46,8 +48,22 @@ function showSearch() {
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
   })
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's';
+  })
+  setTimeout(function () {
+    searchInputEl.focus();
+  }, 600)
 }
 function hideSearch() {
   headerEl.classList.remove('searching');
   document.documentElement.classList.remove('fixed');
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+  })
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's';
+  })
+  searchDelayEls.reverse();
+  searchInputEl.value = '';
 }
